3884d6de2e1cb770c3058befac3a40aef11c4bc0
Nurlan Mukhanov <102@mp.kz>
2019-09-20 00:25:03 +0600

    optimization

--- src/DBDException.php
+++ src/DBDException.php
@@ -6,52 +6,58 @@ use Exception;
 
 class DBDException extends Exception
 {
-    protected $query;
-    protected $trace;
-    protected $fullTrace;
-    protected $shortTrace;
-    protected $message;
-    protected $code;
-    protected $file;
-    protected $line;
+	protected $query;
+	protected $trace;
+	protected $fullTrace;
+	protected $shortTrace;
+	protected $message;
+	protected $code;
+	protected $file;
+	protected $line;
+	protected $arguments;
 
-    public function __construct(string $message = "", $query = null) {
-        $this->query = $query;
-        $this->message = $message;
-        $this->code = E_ERROR;
+	public function __construct(string $message = "", $query = null, $arguments = null) {
+		$this->query = $query;
+		$this->message = $message;
+		$this->code = E_ERROR;
+		$this->arguments = $arguments;
 
-        $backTrace = parent::getTrace();
-        $this->fullTrace = $backTrace;
+		$backTrace = parent::getTrace();
+		$this->fullTrace = $backTrace;
 
-        foreach($backTrace as $trace) {
-            if(isset($trace['file'])) {
-                $pathInfo = pathinfo($trace['file']);
-                if($pathInfo['basename'] == "DBD.php") {
-                    array_shift($backTrace);
-                    continue;
-                }
-                else {
-                    break;
-                }
-            }
-        }
-        $this->file = $backTrace[0]['file'];
-        $this->line = $backTrace[0]['line'];
+		foreach($backTrace as $trace) {
+			if(isset($trace['file'])) {
+				$pathInfo = pathinfo($trace['file']);
+				if($pathInfo['basename'] == "DBD.php") {
+					array_shift($backTrace);
+					continue;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		$this->file = $backTrace[0]['file'];
+		$this->line = $backTrace[0]['line'];
 
-        $this->shortTrace = $backTrace;
+		$this->shortTrace = $backTrace;
 
-        parent::__construct($message, $this->code);
-    }
+		parent::__construct($message, $this->code);
+	}
 
-    public function getFullTrace() {
-        return $this->fullTrace;
-    }
+	public function getArguments() {
+		return $this->arguments;
+	}
 
-    public function getQuery() {
-        return $this->query;
-    }
+	public function getFullTrace() {
+		return $this->fullTrace;
+	}
 
-    public function getShortTrace() {
-        return $this->shortTrace;
-    }
+	public function getQuery() {
+		return $this->query;
+	}
+
+	public function getShortTrace() {
+		return $this->shortTrace;
+	}
 }
